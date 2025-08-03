4c614bc4440d018fb4bd19788b7d136d899a56e5
Anish Pallati <i@anish.land>
2025-08-02 22:54:49 -0400

    feat: allow tauri dev domains in cors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -171,6 +171,8 @@ fn build_cors_layer() -> CorsLayer {
             "https://cmu.quest".parse().unwrap(),
             "https://quest.scottylabs.org".parse().unwrap(),
             "http://localhost:1420".parse().unwrap(),
+            "tauri://localhost".parse().unwrap(), // iOS Tauri
+            "http://tauri.localhost".parse().unwrap(), // Android Tauri
         ])
         .allow_methods([
             Method::GET,

84eef98165f08142a22b0dab5e9a1bb5e60d4b9b
Theodor Urban <theo.s.urban@gmail.com>
2025-08-11 11:25:37 -0400

    convert to closer to figma and update some dorm mascot images

--- frontend/src/routes/dorm-select.tsx
+++ frontend/src/routes/dorm-select.tsx
@@ -22,11 +22,59 @@ export const Route = createFileRoute("/dorm-select")({
 	component: RouteComponent,
 });
 
+const MascotData: Record<DormName, { mascotName: string; chant: string }> = {
+	"Morewood Gardens": {
+		mascotName: "Gardens the Sunflower",
+		chant: '"What do we need?! Morewood!!"',
+	},
+	"Morewood E-Tower": {
+		mascotName: "Yuxiang the Pineapple",
+		chant: "“Who's Got The Power?! E-Tower!!”",
+	},
+	Donner: {
+		mascotName: "Donner the Whale",
+		chant: '"“Yeah, Donner!!”',
+	},
+	Stever: {
+		mascotName: "Stever the Cactus",
+		chant:
+			'"Brrr...It\'s Cold in here... There must be some Stever in the Atmosphere"',
+	},
+	Mudge: {
+		mascotName: "Mudge the Koi",
+		chant: '"Who\'s house? Mudge House!!"',
+	},
+	"Res on Fifth": {
+		mascotName: "Ranch the Flamingo",
+		chant: '"Party on what ave?! Fifth Ave!!"\n~Res on Fifth',
+	},
+	Whesco: {
+		mascotName: "Whesco the Penguin",
+		chant: '"Let\'s Go, Whesco!!"\n~Henderson, Welch, and Scobell',
+	},
+	Hammerschlag: {
+		mascotName: "Penny the HedgeHog",
+		chant: '"Can I get a Hill Yeah?! Hill Yeah!!"\n~Hammerschlag',
+	},
+	"McGill and Boss": {
+		mascotName: "Randal the Red Panda",
+		chant: '"Can I get a Hill Yeah?! Hill Yeah!!"\n~Hammerschlag',
+	},
+	"Margaret Morrison": {
+		mascotName: "Aimee the Magpie",
+		chant: '"What do we need?! Morisson!!"',
+	},
+};
+
 function RouteComponent() {
 	const { from } = Route.useSearch();
 	const { $api } = useApi();
 	const navigate = useNavigate();
 
+	const [selectedDorm, setSelectedDorm] = useState<{
+		name: DormName;
+		image_path: string;
+	} | null>(null);
 	const [error, setError] = useState<string | null>(null);
 
 	const updateDormMutation = $api.useMutation("put", "/api/profile/dorm", {
@@ -47,43 +95,144 @@ function RouteComponent() {
 	};
 
 	return (
-		<div className="px-4 pt-6 max-w-xl mx-auto [view-transition-name:main-content]">
-			<div className="py-2 bg-[#941120] outline-2 outline-black rounded-lg mb-2 text-center text-white">
-				<h1 className="text-2xl font-bold">Select your dorm:</h1>
-			</div>
-			{error && (
-				<div className="py-2 bg-[#941120] outline-2 outline-black rounded-lg mb-2 text-center text-white">
-					{error}
+		<div className="mx-auto bg-white h-screen flex flex-col border border-gray-300 rounded-lg shadow-lg">
+			<div className="px-4 pt-6 pb-2 rounded-t-lg flex-1 content-center">
+				{" "}
+				{/* Explainer + selected image */}
+				<div className="flex justify-center py-2">
+					{" "}
+					{/* Mascot Image */}
+					<img
+						src={
+							selectedDorm
+								? selectedDorm.image_path
+								: "images/dorm-mascots/defaultscotty.png"
+						}
+						alt={`${selectedDorm ? selectedDorm.name : ""} mascot`}
+						className="w-48 h-48 object-contain"
+					/>
 				</div>
-			)}
-			<div className="space-y-3 px-2 py-1 outline-2 rounded-lg bg-[#941120]">
-				{Object.entries(dormGroups).map(([groupName, dorms]) =>
-					dorms.map((dorm) => (
-						<button
-							key={dorm.name}
-							type="button"
-							className={
-								"flex w-full items-center my-4 p-3 outline-2 rounded-lg " +
-								dormColors[groupName as keyof typeof dormColors].light
-							}
-							onClick={() => handleSubmit(dorm.name)}
-						>
-							<img
-								src={`${dorm.image_path}`}
-								alt={""}
-								className={
-									"object-contain w-16 h-16 rounded-lg p-1 " +
-									dormColors[groupName as DormGroup].primary
-								}
-							/>
-							<div className="text-lg font-semibold p-4">
-								{groupName}: {dorm.name}
+				{/* Title & Subtitle */}
+				<div className="text-center px-8 pb-6">
+					<h1 className="text-xl font-semibold mb-2">
+						{selectedDorm
+							? MascotData[selectedDorm.name].mascotName
+							: "Dorm Mascots"}
+					</h1>
+					{selectedDorm ? (
+						<p className="text-gray-600 text-sm max-w-xs mx-auto">
+							{MascotData[selectedDorm.name].chant}
+						</p>
+					) : (
+						<p className="text-gray-600 text-sm max-w-xs mx-auto">
+							Every year, all first year dorms compete in the{" "}
+							<strong>Carnegie Cup to crown the best house!</strong>
+						</p>
+					)}
+				</div>
+			</div>
+			<div className="px-4 py-6 flex-none h-[500px]">
+				{/* Dorm mascots scroll */}
+				<div className="bg-gray-100 w-full rounded-lg py-4 flex-initial shadow-inner overflow-hidden px-4">
+					<div className="flex flex-row overflow-x-scroll gap-4">
+						{Object.entries(dormGroups).map(([groupName, dorms]) => (
+							<div key={groupName}>
+								<div
+									key={groupName}
+									className={
+										"flex flex-row items-center rounded-2xl p-2 pb-1 " +
+										dormColors[groupName as DormGroup].muted
+									}
+								>
+									{dorms.map((dorm) => (
+										<button
+											type="button"
+											className="m-2 overflow-x-auto scrollbar-hide"
+											key={dorm.name}
+											onClick={() => {
+												setSelectedDorm(dorm);
+											}}
+										>
+											<div
+												className={
+													"w-[150px] h-[150px] rounded-2xl p-2 pb-3 " +
+													dormColors[groupName as DormGroup].selected
+												}
+											>
+												<img
+													src={dorm.image_path}
+													alt={`${dorm.name} mascot`}
+													className={
+														"object-contain w-full h-full rounded-2xl " +
+														dormColors[groupName as keyof typeof dormColors]
+															.light
+													}
+												/>
+											</div>
+											<h5 className="block text-center text-md text-white font-semibold h-12 content-center">
+												{dorm.name}
+											</h5>
+										</button>
+									))}
+								</div>
 							</div>
-							<ChevronRight className="ml-auto text-gray-500" />
-						</button>
-					)),
-				)}
+						))}
+					</div>
+				</div>
+
+				{/* Confirm Button */}
+				<div className="px-6 py-6">
+					<button
+						type="button"
+						className="w-full bg-red-700 text-white font-semibold py-3 rounded-full hover:bg-red-800 transition"
+						onClick={() => selectedDorm && handleSubmit(selectedDorm.name)}
+					>
+						Confirm
+					</button>
+				</div>
 			</div>
 		</div>
 	);
 }
+
+// 	return (
+// 		<div className="px-4 pt-6 max-w-xl mx-auto [view-transition-name:main-content]">
+// 			<div className="py-2 bg-[#941120] outline-2 outline-black rounded-lg mb-2 text-center text-white">
+// 				<h1 className="text-2xl font-bold">Select your dorm:</h1>
+// 			</div>
+// 			{error && (
+// 				<div className="py-2 bg-[#941120] outline-2 outline-black rounded-lg mb-2 text-center text-white">
+// 					{error}
+// 				</div>
+// 			)}
+// 			<div className="space-y-3 px-2 py-1 outline-2 rounded-lg bg-[#941120]">
+// 				{Object.entries(dormGroups).map(([groupName, dorms]) =>
+// 					dorms.map((dorm) => (
+// 						<button
+// 							key={dorm.name}
+// 							type="button"
+// 							className={
+// 								"flex w-full items-center my-4 p-3 outline-2 rounded-lg " +
+// 								dormColors[groupName as keyof typeof dormColors].light
+// 							}
+// 							onClick={() => handleSubmit(dorm.name)}
+// 						>
+// 							<img
+// 								src={`${dorm.image_path}`}
+// 								alt={""}
+// 								className={
+// 									"object-contain w-16 h-16 rounded-lg p-1 " +
+// 									dormColors[groupName as DormGroup].primary
+// 								}
+// 							/>
+// 							<div className="text-lg font-semibold p-4">
+// 								{groupName}: {dorm.name}
+// 							</div>
+// 							<ChevronRight className="ml-auto text-gray-500" />
+// 						</button>
+// 					)),
+// 				)}
+// 			</div>
+// 		</div>
+// 	);
+// }
